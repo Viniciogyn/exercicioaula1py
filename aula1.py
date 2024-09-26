78050d7d733a71b7cdf74f4261ef94cff7e07c14
vinicio Barbosa do nascimento <viniieg@icloud.com>
2024-09-25 21:30:10 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,19 @@
+# resposta1 = '\nResposta: \n''È uma linguagem de programação usado para codificação'
+# resposta2 = '\nResposta: \n''Existem diversos compiladores utilizados para entender e interpretar os codigos, um dos exemplos é o Visual studio Code.' 
+# resposta3 = '\nResposta: \n''O Python foi criado em meados de 1990 na holanda para substituir a linguagem ABC.' 
+
+# print('--------------------------------------------------------------------------------------------------')
+# print('pergunta 1')
+# print(f'O que é python?',resposta1)
+
+# print('--------------------------------------------------------------------------------------------------')
+
+# print('pergunta 2')
+# print('Qual o programa (IDE) executa o arquivo .py?',resposta2)
+
+# print('--------------------------------------------------------------------------------------------------')
+
+# print('pergunta 3')
+# print(' Qual a linguagem de programação o Python substituiu?', resposta3)
+
+
